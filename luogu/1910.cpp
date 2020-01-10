1c7950481c3c775beb25d280b4cfd1567a3fd4d7
lanceyu120@gmail.com <lanceyu120@gmail.com>
2020-01-11 01:00:52 +0800

    My ACM/ICPC

---
+++ luogu/1910.cpp
@@ -0,0 +1,74 @@
+/*************************************************************************
+>>> Author: WindCry1
+>>> Mail: lanceyu120@gmail.com
+>>> Website: https://windcry1.com
+>>> Date: 12/30/2019 11:03:37 PM
+*************************************************************************/
+//#pragma GCC optimize(3)
+//#pragma GCC diagnostic error "-std=c++11"
+#include <cstring>
+#include <cmath>
+#include <cstdio>
+#include <cctype>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <queue>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <complex>
+#include <stack>
+#include <bitset>
+#include <iomanip>
+#include <list>
+#include <sstream>
+#include <fstream>
+#if __cplusplus >= 201103L
+#include <unordered_map>
+#include <unordered_set>
+#endif
+#define endl '\n'
+#define ll long long
+#define ull unsigned long long
+#define DEBUG(x) cout<<#x<<" : "<<x<<endl;
+#define lowbit(x) x&(-x)
+#define ls u<<1
+#define rs u<<1|1
+using namespace std;
+template<typename T> inline T MIN(const T &a,const T &b) {return a<b?a:b;}
+template<typename T> inline T MAX(const T &a,const T &b) {return a>b?a:b;}
+template<typename T,typename ...Args> inline T MIN(const T &a,const T &b,Args ...args) {return MIN(MIN(a,b),args...);}
+template<typename T,typename ...Args> inline T MAX(const T &a,const T &b,Args ...args) {return MAX(MAX(a,b),args...);}
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<double,double> pdd;
+const double eps = 1e-8;
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+const int dir[4][2]={-1,0,1,0,0,-1,0,1};
+int a[110],b[110],c[110];
+int dp[1010][1010];
+int main(){
+	ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#ifdef WindCry1
+	freopen("C:\\Users\\LENOVO\\Desktop\\in.txt","r",stdin);
+#endif
+	int n,m,x;cin>>n>>m>>x;
+	for(int i=1;i<=n;i++){
+		cin>>a[i]>>b[i]>>c[i];
+	}
+	for(int i=1;i<=n;i++){
+		for(int j=m;j>=b[i];j--){
+			for(int k=x;k>=c[i];k--){
+				dp[j][k]=max(dp[j][k],dp[j-b[i]][k-c[i]]+a[i]);
+			}
+		}
+	}
+	cout<<dp[m][x]<<endl;
+	return 0;
+}
+
+
