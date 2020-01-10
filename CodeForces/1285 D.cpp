c3de5cd8a09d4f50e826d08635998b6b0a3024aa
lanceyu120@gmail.com <lanceyu120@gmail.com>
2020-01-11 00:04:42 +0800

    My ACM/ICPC

---
+++ CodeForces/1285 D.cpp
@@ -0,0 +1,77 @@
+/*************************************************************************
+>>> Author: WindCry1
+>>> Mail: lanceyu120@gmail.com
+>>> Website: https://windcry1.com
+>>> Date: 12/30/2019 11:03:37 PM
+*************************************************************************/
+//#pragma GCC optimize(3)
+//#pragma GCC diagnostic error "-std=c++11"
+#include <cstring>
+#include <cmath>
+#include <cstdio>
+#include <cctype>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <queue>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <complex>
+#include <stack>
+#include <bitset>
+#include <iomanip>
+#include <list>
+#include <sstream>
+#include <fstream>
+#if __cplusplus >= 201103L
+#include <unordered_map>
+#include <unordered_set>
+#endif
+#define endl '\n'
+#define ll long long
+#define ull unsigned long long
+#define DEBUG(x) cout<<#x<<" : "<<x<<endl;
+#define lowbit(x) x&(-x)
+#define ls u<<1
+#define rs u<<1|1
+using namespace std;
+template<typename T> inline T MIN(const T &a,const T &b) {return a<b?a:b;}
+template<typename T> inline T MAX(const T &a,const T &b) {return a>b?a:b;}
+template<typename T,typename ...Args> inline T MIN(const T &a,const T &b,Args ...args) {return MIN(MIN(a,b),args...);}
+template<typename T,typename ...Args> inline T MAX(const T &a,const T &b,Args ...args) {return MAX(MAX(a,b),args...);}
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<double,double> pdd;
+const double eps = 1e-8;
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+const int dir[4][2]={-1,0,1,0,0,-1,0,1};
+int a[100010];
+int bit[32][2];
+int main(){
+	ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#ifdef WindCry1
+	freopen("C:\\Users\\LENOVO\\Desktop\\in.txt","r",stdin);
+#endif
+	int n;cin>>n;
+	for(int i=0;i<n;i++) {
+		cin>>a[i];
+		for(int j=0;j<30;j++) bit[j][a[i]&(1<<j)?1:0]++;
+	}
+	int res=0;
+#ifdef WindCry1
+	for(int i=0;i<30;i++){
+		cout<<bit[i][0]<<" "<<bit[i][1]<<endl;
+	}
+#endif
+	for(int i=29;~i;i--){
+		if(bit[i][1]) res|=1<<i;
+	}
+	int m=0;
+	for(int i=0;i<n;i++) m=max(m,res^a[i]);
+	cout<<m<<endl;
+	return 0;
+}
