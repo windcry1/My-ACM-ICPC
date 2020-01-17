a3528ec6ca37ffa791d5f74656649f151187d93f
lanceyu120@gmail.com <lanceyu120@gmail.com>
2020-01-17 19:51:45 +0800

    My ACM/ICPC

--- NowCoder/test.cpp
+++ NowCoder/test.cpp
@@ -1,56 +1,30 @@
-/*************************************************************************
->>> Author: WindCry1
->>> Mail: lanceyu120@gmail.com
->>> Website: https://windcry1.com
->>> Date: 12/15/2019 10:50:45 PM
-*************************************************************************/
-#include <cstring>
-#include <cmath>
-#include <cstdio>
-#include <cctype>
-#include <cstdlib>
-#include <ctime>
-#include <vector>
 #include <iostream>
-#include <string>
-#include <queue>
-#include <set>
-#include <map>
-#include <algorithm>
-#include <complex>
-#include <stack>
-#include <bitset>
-#include <iomanip>
-#include <list>
-#include <sstream>
-#include <fstream>
-#if __cplusplus >= 201103L
-#include <unordered_map>
-#include <unordered_set>
-#endif
-#define ll long long
-#define ull unsigned long long
-#define DEBUG(x) cout<<#x<<" : "<<x<<endl;
+#include <cstring>
 using namespace std;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-const double clf = 1e-8;
-const int MMAX = 0x7fffffff;
-const int INF = 0x3f3f3f3f;
-const int mod = 1e9+7;
-const int dir[4][2]={-1,0,1,0,0,-1,0,1};
-ostream& operator <<(ostream &out, pii &p){
-	return out<<p.first<<" "<<p.second;
-}
-istream& operator >>(istream &in, pii &p){
-	return in>>p.first>>p.second;
-}
+int dp[1010][1010],aa[1010],bb[1010],w[1010],v[1010];
 int main(){
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	//ifstream cin("C:\\Users\\LENOVO\\Desktop\\in.txt");
-	//ofstream cout("C:\\Users\\LENOVO\\Desktop\\out.txt");
-	
-	return 0;
+	ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int T;cin>>T;while(T--){
+        int n,m,K;cin>>n>>m>>K;
+        for(int i=1;i<=n;i++) cin>>w[i];
+        for(int i=1;i<=n;i++) cin>>v[i];
+        memset(dp,0,sizeof(dp));
+        for(int i=1;i<=n;i++)
+            for(int j=m;j>=v[i];j--){
+            for(int k=1;k<=K;k++){
+                aa[k]=dp[j-v[i]][k]+w[i];
+                bb[k]=dp[j][k];
+            }
+            aa[K+1]=bb[K+1]=-1;
+            int a=1,b=1,c=1;
+            while(c<=K && (aa[a]!=-1 || bb[b]!=-1)){
+                if(aa[a]>bb[b]) dp[j][c]=aa[a],++a;
+                else dp[j][c]=bb[b],++b;
+                if(dp[j][c]!=dp[j][c-1])
+                    ++c;
+            }
+        }
+    	cout<<dp[m][K]<<endl;
+    }
+    return 0;
 }
