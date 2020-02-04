ba1554c07eef6f2a7a091f2e4ec27b7565aa3ec7
lanceyu120@gmail.com <lanceyu120@gmail.com>
2020-02-04 17:24:54 +0800

    My ACM/ICPC

---
+++ NowCoder/3002 B.cpp
@@ -0,0 +1,66 @@
+/*************************************************************************
+>>> Author: WindCry1
+>>> Mail: lanceyu120@gmail.com
+>>> Website: https://windcry1.com
+>>> Date: 12/30/2019 11:03:37 PM
+*************************************************************************/
+//#pragma GCC optimize(3)
+//#pragma GCC diagnostic error "-std=c++11"
+#include <cstring>
+#include <cmath>
+#include <cstdio>
+#include <cctype>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <queue>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <complex>
+#include <stack>
+#include <bitset>
+#include <iomanip>
+#include <list>
+#include <sstream>
+#include <fstream>
+#if __cplusplus >= 201103L
+#include <unordered_map>
+#include <unordered_set>
+#endif
+#define endl '\n'
+#define ALL(x) x.begin(),x.end()
+#define ll long long
+#define ull unsigned long long
+#ifdef WindCry1
+#define DEBUG(x) cout<<#x<<" : "<<x<<endl;
+#endif
+#define lowbit(x) x&(-x)
+#define ls u<<1
+#define rs u<<1|1
+using namespace std;
+template<typename T> inline T MIN(const T &a,const T &b) {return a<b?a:b;}
+template<typename T> inline T MAX(const T &a,const T &b) {return a>b?a:b;}
+template<typename T,typename ...Args> inline T MIN(const T &a,const T &b,Args ...args) {return MIN(MIN(a,b),args...);}
+template<typename T,typename ...Args> inline T MAX(const T &a,const T &b,Args ...args) {return MAX(MAX(a,b),args...);}
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<double,double> pdd;
+const double eps = 1e-8;
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+const int dir[4][2]={-1,0,1,0,0,-1,0,1};
+
+int main(){
+	ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#ifdef WindCry1
+	freopen("C:/Users/LENOVO/Desktop/in.txt","r",stdin);
+#endif
+	double n,x,a,b;cin>>n>>x>>a>>b;
+	cout<<fixed<<setprecision(2)<<(n*a*x/100+n*b*(100-x)/100)<<endl;
+	return 0;
+}
+
+
